a07a653eb388dde3e94d058a2d8e63427d4cc720
acpg <acperezgea@gmail.com>
2017-11-29 22:28:04 -0500

    modified impala file for final output

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ The raw data gets cleaned with the MapReduce on the cleaner folder.
 The cleaned data gets profiled with MapReduce on the profiler folder.
 
 The sql file then creates a table in impala for our analytic.
+Output the file in a csv file with the command 
+impala-shell -B -q 'select * from reddit_final' -o data/reddit_final.csv --print_header '--outputdelimiter=,'
 
 
 
--- reddit_impala.sql
+++ reddit_impala.sql
@@ -4,12 +4,14 @@ DROP TABLE IF EXISTS reddit_table;
 -- The EXTERNAL clause means the data is located outside the central location
 -- for Impala data files and is preserved when the associated Impala table is dropped.
 -- We expect the data to already exist in the directory specified by the LOCATION clause.
-CREATE EXTERNAL TABLE reddit
+CREATE EXTERNAL TABLE reddit_table
 (
-   year INT,
-   month INT,
+   year STRING,
+   month STRING,
    word STRING,
    count INT
 )
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
 LOCATION '/user/cloudera/redditprofiler/output';
+
+CREATE TABLE reddit_final AS SELECT date_sub(add_months(cast(concat_ws('-',year,month,'01') as timestamp),1),1) as date_month, word, count from reddit_table;
