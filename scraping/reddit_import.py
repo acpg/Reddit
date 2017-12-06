6ed5ca228b1b5eff42655a8b17867fc43da03e04
acpg <acperezgea@gmail.com>
2017-12-06 12:23:59 -0500

    cleaner ignores 100 common words

--- scraping/reddit_import.py
+++ scraping/reddit_import.py
@@ -2,24 +2,37 @@ import json
 import praw
 from praw.models import MoreComments
 from datetime import datetime
+from datetime import timedelta
 import credentials
 # import your own credentials with
 # reddit = praw.Reddit(client_id='my client id', client_secret='my client secret', user_agent='my user agent')
 
 reddit = credentials.mycredentials()
-
-with open('../data/reddit_raw', 'w') as output:
-	for submission in reddit.subreddit('bitcoin').top('all'): 
-		list_of_items = []
-		list_of_items.append(submission.title.encode("utf-8"))
-		list_of_items.append(datetime.fromtimestamp(submission.created_utc).isoformat())
-		list_of_items.append(submission.score)
-		list_of_items.append(submission.num_comments)
-		list_of_comments = []
-		for comment in submission.comments:
-			if isinstance(comment, MoreComments):
-				continue
-			list_of_comments.append(comment.body.encode("utf-8"))
-		list_of_items.append(list_of_comments)
-		json.dump(list_of_items,output)
-		output.write('\n')
+# date looping modified from giltay 2008
+# https://stackoverflow.com/questions/153584/how-to-iterate-over-a-timespan-after-days-hours-weeks-and-months-in-python
+start_date = datetime(2015,1,11,0,0,0)
+#end_date = datetime(2011,1,1,0,0,0)
+#month_step = 1
+delta = timedelta(days=1)
+current_date = start_date
+for i in range(0,1000):
+	#carry, new_month = divmod(current_date.month - 1 + month_step, 12)
+	#new_month += 1
+	#new_date = current_date.replace(year=current_date.year + carry,month=new_month)
+	new_date = current_date + delta
+	with open('../data/reddit_raw'+current_date.strftime("%Y%m%d"), 'w') as output:
+		for submission in reddit.subreddit('bitcoin').submissions(int(current_date.strftime('%s')),int(new_date.strftime('%s'))-1): 
+			list_of_items = []
+			list_of_items.append(submission.title.encode("utf-8"))
+			list_of_items.append(datetime.fromtimestamp(submission.created_utc).isoformat())
+			list_of_items.append(submission.score)
+			list_of_items.append(submission.num_comments)
+			list_of_comments = []
+			for comment in submission.comments:
+				if isinstance(comment, MoreComments):
+					continue
+				list_of_comments.append(comment.body.encode("utf-8"))
+			list_of_items.append(list_of_comments)
+			json.dump(list_of_items,output)
+			output.write('\n')
+	current_date = new_date
