db89d2f872519859179b3446827493c90a3fe2ff
acpg <acperezgea@gmail.com>
2017-12-07 17:32:55 -0500

    remove if no comments

--- cleaner/CleanMapper.java
+++ cleaner/CleanMapper.java
@@ -11,19 +11,22 @@ public class CleanMapper extends Mapper<LongWritable, Text, Text, Text> {
 public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 	String line = (value.toString()).toLowerCase().substring(1);
 	int ii = line.indexOf(", [\"");
-	String title = line.substring(0,ii);
-	String[] titles = title.split("\", \"");
-	titles[0] = titles[0].replaceAll("\"|\\\"|'|`","");
-	titles[1] = titles[1].replaceAll("\"","");
-	titles[0] = titles[0].replaceAll("[^a-z0-9]"," ");
-	titles[0] = titles[0].trim();
-	title = titles[0]+", "+titles[1];
-	line = line.substring(ii+1);
-	String[] words = line.split(" ");
-	for(String word:words) {
-		word = word.replaceAll("\"|\\\"|'|`","");
-		word = word.replaceAll("[^a-z0-9]", " ").trim();
-		context.write(new Text(title), new Text(word));
+	// Drop if no comments
+	if(-1!=ii) {
+		String title = line.substring(0,ii);
+		String[] titles = title.split("\", \"");
+		titles[0] = titles[0].replaceAll("\"|\\\"|'|`","");
+		titles[1] = titles[1].replaceAll("\"","");
+		titles[0] = titles[0].replaceAll("[^a-z0-9]"," ");
+		titles[0] = titles[0].trim();
+		title = titles[0]+", "+titles[1];
+		line = line.substring(ii+1);
+		String[] words = line.split(" ");
+		for(String word:words) {
+			word = word.replaceAll("\"|\\\"|'|`","");
+			word = word.replaceAll("[^a-z0-9]", " ").trim();
+			context.write(new Text(title), new Text(word));
+		}
 	}
 }
 }
