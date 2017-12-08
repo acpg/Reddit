a644206f08439ed323b30b0027731daaef3e6137
acpg <acperezgea@gmail.com>
2017-12-08 17:32:33 -0500

    dumbo adaptation

--- reddit_impala.sql
+++ reddit_impala.sql
@@ -1,20 +1,20 @@
 -- Modified from cloudera.com
-
 DROP TABLE IF EXISTS reddit_table;
--- The EXTERNAL clause means the data is located outside the central location
--- for Impala data files and is preserved when the associated Impala table is dropped.
--- We expect the data to already exist in the directory specified by the LOCATION clause.
 CREATE EXTERNAL TABLE reddit_table
 (
-   year STRING,
    month STRING,
    word STRING,
+   score INT,
    count INT
 )
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
-LOCATION '/user/cloudera/redditprofiler';
+LOCATION '/user/apg367/redditprofiler';
 
-DROP TABLE IF EXISTS reddit_final;
-CREATE TABLE reddit_final AS SELECT date_sub(add_months(cast(concat_ws('-',year,month,'01') as timestamp),1),1) as date_month, word, count 
-	FROM reddit_table WHERE count>2;
+DROP TABLE IF EXISTS reddit;
+CREATE TABLE reddit AS 
+-- modified from https://community.hortonworks.com/questions/24667/hive-top-n-records-within-a-group.html
+SELECT * FROM (
+SELECT month, word, count, score, 
+rank() over ( partition by month ORDER BY count DESC) AS rank 
+FROM reddit_table ) t WHERE rank <= 10; --top ten words
 
